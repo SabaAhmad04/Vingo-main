f11a5d1f7d600df80043f2e87a801fe514fcd915
SabaAhmad04 <sabaahmad935@gmail.com>
2026-04-29 00:21:32 +0530

    eightth commit

--- Vingo-main/backend/index.js
+++ Vingo-main/backend/index.js
@@ -54,7 +54,7 @@ const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("/*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
