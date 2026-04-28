024c53dee7c390dfa2ad97febb264ebe83919624
SabaAhmad04 <sabaahmad935@gmail.com>
2026-04-28 23:43:04 +0530

    third commit

--- Vingo-main/frontend/vite.config.js
+++ Vingo-main/frontend/vite.config.js
@@ -6,6 +6,10 @@
 //   plugins: [react(),tailwindcss()],
 // })
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
